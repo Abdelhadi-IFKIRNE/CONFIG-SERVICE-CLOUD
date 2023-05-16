43d9fb3060a2e705a97142af3fdacaae318c109b
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-16 10:34:41 +0000

    config service cloud updates

---
+++ src/main/java/com/berexia/configservicecloud/ConfigServiceCloudApplication.java
@@ -0,0 +1,15 @@
+package com.berexia.configservicecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berexia/configservicecloud/ConfigServiceCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berexia.configservicecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
